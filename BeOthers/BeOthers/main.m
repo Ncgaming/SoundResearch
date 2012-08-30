7c79e92dfd2c60fd3584981335c3c1f778647423
Ncgaming <yang19910207@sina.com>
2012-08-30 18:44:02 +0800

    initialize Project-BeOthers

---
+++ BeOthers/BeOthers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BeOthers
+//
+//  Created by Ncgaming on 30/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
